55b5202fb2ff1b202fb52f30bb7d77c417d7299b
brodywhalen <123722295+brodywhalen@users.noreply.github.com>
2023-10-24 00:08:20 -0500

    can now move between notes, but stull need to be droppable on empty

--- website/src/components/Notes.js
+++ website/src/components/Notes.js
@@ -1,47 +1,70 @@
 import { useState, useRef } from "react"
 const dummyNotes = [
-    'hello world',
-    'testing',
-    'first project on my own!'
+    [
+        'hello world',
+        'testing',
+        'first project on my own!'
+    ],
+    [
+        'finished column',
+        'gay sex is gay',
+        'I\'m going to get a sale'
+    ]
 ]
+ 
 
 const Notes = () => {
 
     const dragItem = useRef()
     const dragOverItem = useRef()
+    const dragOverItemColumn = useRef()
 
     const[notes, setNotes] = useState(dummyNotes)
+    
 
     const addNote = (event) => {
         event.preventDefault()
         const addedNote = event.target.addedNote.value
         event.target.addedNote.value = ''
         console.log("notes after button push ", notes)
-        notes.push(addedNote)
+        notes[0].push(addedNote)
         const newNoteList = [...notes]
         console.log("newNoteList: ", newNoteList)
         setNotes(newNoteList)
     
     }
-    const dragStart = (e, index) => {
+    const dragStart = (e, index, column) => {
         dragItem.current = index
-        console.log(e.target.innerHTML)
+        console.log('drag item index: ', dragItem.current)
+        //console.log(e.target.innerHTML)
     }
-    const dragEnter = (e, index) => {
+    const dragEnter = (e, index, column) => {
         dragOverItem.current = index
-        console.log(e.target.innerHTML)
+        dragOverItemColumn.current = column
+        //console.log(e.target.innerHTML)
     }
-    const drop = (e) => {
+    const drop = (e, column) => {
         const notesCopy = [...notes]
         //console.log('drag item current :', dragItem)
-        const dragItemContent = notesCopy[dragItem.current]
-        //console.log('drag item content :', dragItemContent)
-        notesCopy.splice(dragItem.current,1)
-        notesCopy.splice(dragOverItem.current,0,dragItemContent)
+        const dragItemContent = notesCopy[column][dragItem.current]
+        console.log('drag item content :', dragItemContent)
+        notesCopy[column].splice(dragItem.current,1)
+        console.log("drag over column: ", dragOverItemColumn)
+        notesCopy[dragOverItemColumn.current].splice(dragOverItem.current,0 , dragItemContent)
+        console.log("notes copy: ", notesCopy)
         dragItem.current = null
         dragOverItem.current = null
+        dragOverItemColumn.current = null
         setNotes(notesCopy)
 
+    }
+    const backDrop = (e, column) => {
+
+
+    }
+    const dragBackEnter = (e, column) => {
+
+        
     }
 
     console.log('notes at start: ', notes)
@@ -56,15 +79,34 @@ const Notes = () => {
                     create
                 </button>
             </form>
-            <div style = {{backgroundColor:'lightblue', margin:'20px 25%', textAlign:'left', fontSize:'24px'}}>
-                {notes.map((note,index) =>
-        
-                    <ol key = {index} onDragStart={(e) => dragStart(e, index)} onDragEnter={ (e) => dragEnter(e, index) } onDragEnd={drop} draggable>
-                            {note}
-                            <Button index = {index} notes = {notes} setNotes = {setNotes} />
-                    </ol>
+            <div style = {{display: 'flex'}}>
+                <div onDragEnter={ (e) => dragBackEnter(e,0) } onDragEnd={(e) => backDrop(e, 0)}droppable style = {{backgroundColor:'lightblue',  textAlign:'left', fontSize:'24px' ,flex: '50%'}}>
+                    {notes[0].map((note,index) =>
                     
-                )}
+                        <ol key = {index} onDragStart={(e) => dragStart(e, index, 0)} onDragEnter={ (e) => dragEnter(e, index,0) } onDragEnd={(e) => drop(e, 0)} draggable style = {{border: "solid",display: 'flex'}}>
+                                <div style = {{flex: '50%'}}>
+                                    {note}
+                                </div>
+                                <div style = {{alignSelf: 'right', padding: '10px'}}>
+                                    <Button index = {index} notes = {notes} setNotes = {setNotes} />
+                                </div>
+                        </ol>
+                        
+                    )}
+                </div>
+                <div style = {{backgroundColor:'darkblue', textAlign:'left', fontSize:'24px', flex: '50%'}}>
+                    {notes[1].map((note,index) =>
+                        
+                        <ol key = {index} onDragStart={(e) => dragStart(e, index,1)} onDragEnter={ (e) => dragEnter(e, index,1) } onDragEnd={(e) => drop(e,1)} draggable style = {{border: "solid",display: 'flex'}}>
+                                <div style = {{flex: '50%'}}>
+                                    {note}
+                                </div>
+                                <div style = {{alignSelf: 'right', padding: '10px'}}>
+                                    <Button index = {index} notes = {notes} setNotes = {setNotes} />
+                                </div>
+                        </ol>
+                    )}        
+                </div>
             </div>
         </div>
      
@@ -74,12 +116,12 @@ const Notes = () => {
 const Button = (props) => {
 
     const deleteNote = () => {
-        
-        props.notes.splice(props.index, 1)
-        props.setNotes([...props.notes])
+        const notesCopy = [...props.notes]
+        notesCopy.splice(props.index, 1)
+        props.setNotes([notesCopy])
     }
     return(
-            <button onClick={deleteNote}> delete </button>
+            <button style = {{display: 'inline'}}onClick={deleteNote}> delete </button>
     )
 }
 
